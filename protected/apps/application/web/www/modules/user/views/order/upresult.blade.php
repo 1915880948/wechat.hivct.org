2bf58d86e171ec3e46e34bef2ed43c50d433097c
gouki <gouki.xiao@gmail.com>
2018-01-18 02:35:39 +0800

    try

--- protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
+++ protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
@@ -50,7 +50,7 @@
 
 @push('foot-script')
   {{--<script src="{{yStatic('qiniu/moxie.dev.js')}}"></script>--}}
-  <script src="{{yStatic('qiniu/plupload.dev.js')}}"></script>
+  <script src="{{yJsFileVer(yStatic('qiniu/plupload.dev.js'),env('ASSET_VERSION'))}}"></script>
   <script src="{{yStatic('qiniu/qiniu2.min.js') }}"></script>
   {{--<script src="{{yStatic('qiniu/progress.js')}}"></script>--}}
   <script>

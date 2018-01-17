0307a6f9db78f4fe8e9083f975e12d596a167d90
gouki <gouki.xiao@gmail.com>
2018-01-17 19:32:13 +0800

    更新

--- protected/apps/application/web/www/modules/user/views/order/detail.blade.php
+++ protected/apps/application/web/www/modules/user/views/order/detail.blade.php
@@ -36,46 +36,47 @@
             </div>
         </div>
     </div>
-    <div class="weui-cells weui-cells_form">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                <div class="weui-uploader">
-                    <div class="weui-uploader__hd">
-                        <p class="weui-uploader__title">支付宝帐号：</p>
-                        <div class="weui-uploader__info">
-                            @if( count($images)>0 )
-                                <input type="text" name="alipay" class="alipay" value="{{$orderData['alipay']}}">
-                            @else
-                                <input type="text" name="alipay" class="alipay" placeholder="请正确输入支付宝帐号">
-                            @endif
-                        </div>
-                    </div>
-                    <div class="weui-uploader__bd">
-                        <div class="weui"> 自检结果图片：</div>
-                        <ul class="weui-uploader__files" id="uploaderFiles">
-                            @foreach( $images as $item)
-                                <li class="weui-uploader__file weui-uploader__file_status">
-                                    <div class="weui-uploader__file-content"><img src="{{ $item['image'] }}" width="80" height="80"></div>
-                                </li>
-                            @endforeach
-                        </ul>
-                        <div id="fileArr">
-                            <form id="file_form" enctype="multipart/form-data">
-                                @foreach( $images as $item)
-                                    <input type="hidden" name="images[]" value="{{ $item['image'] }}">
-                                @endforeach
-                                <input type="hidden" name="token">
-                            </form>
+
+    {{--<div class="weui-cells weui-cells_form">--}}
+        {{--<div class="weui-cell">--}}
+            {{--<div class="weui-cell__bd">--}}
+                {{--<div class="weui-uploader">--}}
+                    {{--<div class="weui-uploader__hd">--}}
+                        {{--<p class="weui-uploader__title">支付宝帐号：</p>--}}
+                        {{--<div class="weui-uploader__info">--}}
+                            {{--@if( count($images)>0 )--}}
+                                {{--<input type="text" name="alipay" class="alipay" value="{{$orderData['alipay']}}">--}}
+                            {{--@else--}}
+                                {{--<input type="text" name="alipay" class="alipay" placeholder="请正确输入支付宝帐号">--}}
+                            {{--@endif--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="weui-uploader__bd">--}}
+                        {{--<div class="weui"> 自检结果图片：</div>--}}
+                        {{--<ul class="weui-uploader__files" id="uploaderFiles">--}}
+                            {{--@foreach( $images as $item)--}}
+                                {{--<li class="weui-uploader__file weui-uploader__file_status">--}}
+                                    {{--<div class="weui-uploader__file-content"><img src="{{ $item['image'] }}" width="80" height="80"></div>--}}
+                                {{--</li>--}}
+                            {{--@endforeach--}}
+                        {{--</ul>--}}
+                        {{--<div id="fileArr">--}}
+                            {{--<form id="file_form" enctype="multipart/form-data">--}}
+                                {{--@foreach( $images as $item)--}}
+                                    {{--<input type="hidden" name="images[]" value="{{ $item['image'] }}">--}}
+                                {{--@endforeach--}}
+                                {{--<input type="hidden" name="token">--}}
+                            {{--</form>--}}
                             {{--<input class="weui-uploader__input uploader" id="uploader">--}}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @if( gPayStatus($orderData['pay_status']) == '已支付' && (gOrderStatus($orderData['pay_status'])=='已发货' || gOrderStatus($orderData['pay_status'])=='已收货') )
-        <a href="{{yUrl(['order/upresult','uuid'=>$orderData['uuid']])}}" class="weui-btn weui-btn_primary">上传自检结果</a>
-    @endif
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--@if( gPayStatus($orderData['pay_status']) == '已支付' && (gOrderStatus($orderData['pay_status'])=='已发货' || gOrderStatus($orderData['pay_status'])=='已收货') )--}}
+        {{--<a href="{{yUrl(['order/upresult','uuid'=>$orderData['uuid']])}}" class="weui-btn weui-btn_primary">上传自检结果</a>--}}
+    {{--@endif--}}
 @stop
 
 @push('foot-script')
--- protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
+++ protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
@@ -36,7 +36,7 @@
               <form id="file_form" enctype="multipart/form-data">
                 <input type="hidden" name="token">
               </form>
-              <input class="weui-uploader__input uploader" id="uploader" onfocus="this.blur()">
+              <input class="weui-uploader__input uploader" id="uploader" type="file" accept="image/*" multiple="" onfocus="this.blur()">
             </div>
           </div>
         </div>

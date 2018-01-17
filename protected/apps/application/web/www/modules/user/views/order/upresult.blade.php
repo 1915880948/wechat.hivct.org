aa0148f1c5dc2ba7dc93a89e24a60c67d1dcd9ce
gouki <gouki.xiao@gmail.com>
2018-01-18 01:57:53 +0800

    jssdk

--- protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
+++ protected/apps/application/web/www/modules/user/views/order/upresult.blade.php
@@ -36,18 +36,15 @@
               <form id="file_form" enctype="multipart/form-data">
                 <input type="hidden" name="token">
               </form>
-              {{--<span class="weui-uploader__input uploader" id="uploader" type="file" accept="image/*">--}}
+              <input class="weui-uploader__input uploader" id="uploader" type="file" accept="image/*">
             </div>
           </div>
-          {{--<span class="weui-uploader__input uploader" id="uploader" type="file" accept="image/*">--}}
-          <img id="uploader" class="weui-uploader__input uploader" src="xx"/>
         </div>
       </div>
     </div>
   </div>
   {{--    @if( gPayStatus($orderData['pay_status']) == '已支付' && (gOrderStatus($orderData['pay_status'])=='已发货' || gOrderStatus($orderData['pay_status'])=='已收货') )--}}
-  <a href="javascript:;" class="weui-btn weui-btn_primary apply_back" style="width:50%">保存</a>
-  <a href="javascript:;" class="weui-btn weui-btn_primary up">上传</a>
+  <a href="javascript:;" class="weui-btn weui-btn_primary apply_back">保存</a>
   {{--@endif--}}
 @stop
 
@@ -57,7 +54,7 @@
   <script src="{{yStatic('qiniu/progress.js')}}"></script>
   <script>
       $(function () {
-          // order_list();
+          order_list();
           $(".apply_back").click(function () {
               var images = '';
               $('input[name="images[]"]').each(function () {
@@ -83,7 +80,7 @@
 
       });
       var uploader = Qiniu.uploader({
-          runtimes: 'html5,html4',      // 上传模式，依次退化
+          runtimes: 'html5,flash,html4',      // 上传模式，依次退化
           browse_button: 'uploader',          // 上传选择的点选按钮，必需
           uptoken_url: "{{yUrl(['/site/uptoken'])}}",         // Ajax请求uptoken的Url，强烈建议设置（服务端提供）
           unique_names: true, // 默认 false，key为文件名。若开启该选项，SDK为自动生成上传成功后的key（文件名）。
@@ -93,8 +90,8 @@
           max_file_size: '50mb',             // 最大文件体积限制
           max_retries: 3,                     // 上传失败最大重试次数
           dragdrop: false,                    // 开启可拖曳上传
-          chunk_size: '0mb',                  // 分块上传时，每块的体积
-          auto_start: false,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
+          chunk_size: '1mb',                  // 分块上传时，每块的体积
+          auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传
           multi_selection: false,
           filters: {
               mime_types: [ //只允许上传文件格式
@@ -122,30 +119,30 @@
               },
               'FileUploaded': function (up, file, info) {
                   alert(info);
-                  //                   layer.msg('上传成功', {time: 1200}, function () {
-                  //                   });
-                  //                   var res = JSON.parse(info);
-                  //                   // 查看简单反馈
-                  //                   var domain = up.getOption('domain');
-                  //                   var sourceLink = domain + "/" + res.key; //获取上传成功后的文件的Url
-                  //                   var imgLink = Qiniu.imageView2({
-                  //                       mode: 0,  // 缩略模式，共6种[0-5]
-                  //                       w: 150,   // 具体含义由缩略模式决定
-                  //                       h: 150   // 具体含义由缩略模式决定
-                  // //                        q: 60,   // 新图的图像质量，取值范围：1-100
-                  // //                        format: 'webp'  // 新图的输出格式，取值范围：jpg，gif，png，webp等
-                  //                   }, res.key);
-                  //
-                  //                   var html_file = '<li class="weui-uploader__file weui-uploader__file_status" >\n' +
-                  //                       '                        <div class="weui-uploader__file-content"><img src="' + imgLink + '"></div>\n' +
-                  //                       '                        </li>';
-                  //                   var input_file = ' <input type="hidden" name="images[]" value="' + sourceLink + '">';
-                  //                   if ($('#uploaderFiles li').length > 5) {
-                  //                       $.toast('最多上传4张图片！')
-                  //                   } else {
-                  //                       $('#uploaderFiles').append(html_file);
-                  //                       $("#file_form").append(input_file);
-                  //                   }
+//                   layer.msg('上传成功', {time: 1200}, function () {
+//                   });
+//                   var res = JSON.parse(info);
+//                   // 查看简单反馈
+//                   var domain = up.getOption('domain');
+//                   var sourceLink = domain + "/" + res.key; //获取上传成功后的文件的Url
+//                   var imgLink = Qiniu.imageView2({
+//                       mode: 0,  // 缩略模式，共6种[0-5]
+//                       w: 150,   // 具体含义由缩略模式决定
+//                       h: 150   // 具体含义由缩略模式决定
+// //                        q: 60,   // 新图的图像质量，取值范围：1-100
+// //                        format: 'webp'  // 新图的输出格式，取值范围：jpg，gif，png，webp等
+//                   }, res.key);
+//
+//                   var html_file = '<li class="weui-uploader__file weui-uploader__file_status" >\n' +
+//                       '                        <div class="weui-uploader__file-content"><img src="' + imgLink + '"></div>\n' +
+//                       '                        </li>';
+//                   var input_file = ' <input type="hidden" name="images[]" value="' + sourceLink + '">';
+//                   if ($('#uploaderFiles li').length > 5) {
+//                       $.toast('最多上传4张图片！')
+//                   } else {
+//                       $('#uploaderFiles').append(html_file);
+//                       $("#file_form").append(input_file);
+//                   }
 
               },
               'Error': function (up, err, errTip) {
@@ -167,9 +164,6 @@
               }
           }
       });
-      $('#up').on('click', function () {
-          uploader.start();
-      })
 
       function order_list() {
           $.post("{{ yUrl(['order/payback']) }}", {'method': 'list'}, function (res) {

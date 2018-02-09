117278c2abf8139d46dbf61b8ca51603e9794380
gouki <gouki.xiao@gmail.com>
2018-02-09 11:25:03 +0800

    add db save log

--- protected/apps/application/web/admin/modules/order/views/site/detail.blade.php
+++ protected/apps/application/web/admin/modules/order/views/site/detail.blade.php
@@ -19,7 +19,7 @@
         <div class="col-xs-6">微信订单号：{{ ($order_data['wx_transaction_id']) }}</div>
       <div class="col-xs-6">真实姓名：{{ $survey['name'] or '' }} (微信呢称：<a href="{{yUrl(['/user/lists/detail','uid'=>$userdata['uid']])}}" target="_blank">{{$userdata['realname']}}</a>)</div>
       <div class="col-xs-3"></div>
-      <div class="col-xs-3"><a href="{{yUrl(['/survey/site/detail','uuid'=>$order_data['source_uuid']])}}" target="_blank">查看调研</a></div>
+      <div class="col-xs-3"><a class="btn" href="{{yUrl(['/survey/site/detail','uuid'=>$order_data['source_uuid']])}}" target="_blank">查看调研</a></div>
       <br/>
       {{--<div class="col-xs-3">订单标题：{{ $order_data['info'] }}</div>--}}
       <div class="col-xs-2">订单时间：{{ ($order_data['created_at']) }}</div>

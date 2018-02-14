a5319224c4d8d377c2907c0eceda4796a3881882
gouki <gouki.xiao@gmail.com>
2018-02-15 06:59:54 +0800

    更新菜单权限

--- protected/apps/application/web/www/modules/user/controllers/UpcheckController.php
+++ protected/apps/application/web/www/modules/user/controllers/UpcheckController.php
@@ -11,7 +11,7 @@ namespace application\web\www\modules\user\controllers;
 use application\web\www\modules\user\components\UserBaseController;
 use common\core\base\controller\ActionsTrait;
 
-class Upcheck2Controller extends UserBaseController
+class UpcheckController extends UserBaseController
 {
     use ActionsTrait;
 }

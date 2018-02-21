e1d8aa8d44d5101f0d95b02d0763f9ef71847848
gouki <gouki.xiao@gmail.com>
2018-02-22 00:18:27 +0800

    更新模型！

---
+++ protected/apps/application/web/admin/modules/model/ModelModule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @category ModelModule
+ * @author   gouki <gouki.xiao@gmail.com>
+ * @created 22/02/2018 00:03
+ * @since
+ */
+
+namespace application\web\admin\modules\model;
+
+use yii\base\Module;
+
+class ModelModule extends Module
+{
+}
---
+++ protected/apps/application/web/admin/modules/model/config.php
@@ -0,0 +1,8 @@
+<?php
+
+use application\web\admin\modules\model\ModelModule;
+
+return [
+    'id'    => 'model',
+    'class' => ModelModule::className()
+];

250611b62a443d13ea6afb67c86a83167cc6c2da
gouki <gouki.xiao@gmail.com>
2018-02-21 11:57:26 +0800

    更新菜单权限

--- protected/apps/application/web/www/modules/user/controllers/order/UpResultAction.php
+++ protected/apps/application/web/www/modules/user/controllers/order/UpResultAction.php
@@ -4,17 +4,16 @@ namespace application\web\www\modules\user\controllers\order;
 
 use application\models\base\OrderList;
 use application\web\www\components\WwwBaseAction;
-use common\core\session\GSession;
 
 class UpResultAction extends WwwBaseAction
 {
+    /**
+     * 获取订单
+     * @param string $uuid
+     * @return string
+     */
     public function run($uuid = '')
     {
-        if(GSession::get('debug') == true){
-            echo "<pre>";
-            print_r($this->account);
-            echo "</pre>";
-        }
         $orderList = OrderList::getLastMonthOrder($this->account['uid']);
 
         return $this->render(compact('orderData', 'detailData', 'orderList'));

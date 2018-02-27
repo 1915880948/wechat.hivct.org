6e00c72a6d99a5d658f2fd8e236b962c6486f34f
gouki <gouki.xiao@gmail.com>
2018-02-27 23:36:27 +0800

    更新模型！

--- protected/apps/application/web/admin/modules/order/views/site/deal.blade.php
+++ protected/apps/application/web/admin/modules/order/views/site/deal.blade.php
@@ -39,112 +39,112 @@
                 图片尚未上传（选择图片上传-->这里允许多文件上传）
               </div>
             @endforelse
-                <div class="input_images"></div>
-                <div class="col-md-3" style="margin: 10px;">
-                    <button class="up_images" id="up_images_uploader" style="background: #ede6ff;">+上传</button>
-                    <button class="save_up_images" id="save_up_images" style="background: #ede6ff;">保存</button>
-                </div>
+            <div class="input_images"></div>
+            <div class="col-md-3" style="margin: 10px;">
+              <button class="up_images" id="up_images_uploader" style="background: #ede6ff;">+上传</button>
+              <button class="save_up_images" id="save_up_images" style="background: #ede6ff;">保存</button>
+            </div>
             @if($images)
-                <div class="form-group col-md-12" style="margin: 10px;">
-                  <label class="col-md-2 control-label">订单审核</label>
-                  <div class="col-md-3">
-                    <select class="form-control apply_result" id="apply_result">
-                      <option value="0" {{$orderData['is_to_examine']==0?'selected':''}}>未审核</option>
-                      <option value="1" {{$orderData['is_to_examine']==1?'selected':''}}>通过</option>
-                      <option value="2" {{$orderData['is_to_examine']==2?'selected':''}}>未通过</option>
-                    </select>
-                  </div>
-                  <div class="col-md-3">
-                    <textarea class="form-control examine_reason" id="examine_reason" cols="9" rel="3" placeholder="未通过原因" style="display: none;">{{$orderData['examine_reason']}}</textarea>
-                  </div>
-                  <label class="col-md-3 control-label">
-                    <input type="button" class="input-group-btn btn btn-default btn-sm input-small examine_save" value="保存" style="background: #3fd5c0;color: #ffffff;">
-                  </label>
+              <div class="form-group col-md-12" style="margin: 10px;">
+                <label class="col-md-2 control-label">订单审核</label>
+                <div class="col-md-3">
+                  <select class="form-control apply_result" id="apply_result">
+                    <option value="0" {{$orderData['is_to_examine']==0?'selected':''}}>未审核</option>
+                    <option value="1" {{$orderData['is_to_examine']==1?'selected':''}}>通过</option>
+                    <option value="2" {{$orderData['is_to_examine']==2?'selected':''}}>未通过</option>
+                  </select>
                 </div>
+                <div class="col-md-3">
+                  <textarea class="form-control examine_reason" id="examine_reason" cols="9" rel="3" placeholder="未通过原因" style="display: none;">{{$orderData['examine_reason']}}</textarea>
+                </div>
+                <label class="col-md-3 control-label">
+                  <input type="button" class="input-group-btn btn btn-default btn-sm input-small examine_save" value="保存" style="background: #3fd5c0;color: #ffffff;">
+                </label>
+              </div>
             @endif
           </div>
-      </div>
+        </div>
       </div>
     </div>
   </div>
   <div class="row">
-      @if($orderData['is_to_examine'] == 1 )
-    <div class="col-xs-6">
-      <div class="portlet blue-hoki box">
-        <div class="portlet-title">
-          <div class="caption">
-            <i class="fa fa-cogs"></i>更新检测结果
+    @if($orderData['is_to_examine'] == 1 )
+      <div class="col-xs-6">
+        <div class="portlet blue-hoki box">
+          <div class="portlet-title">
+            <div class="caption">
+              <i class="fa fa-cogs"></i>更新检测结果
+            </div>
+            <div class="actions"></div>
           </div>
-          <div class="actions"></div>
-        </div>
-        <div class="portlet-body">
-          <div class="row static-info">
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">支付宝账号</label>
-              <div class="col-md-6">
-                {{ $orderData['alipay'] }}
+          <div class="portlet-body">
+            <div class="row static-info">
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">支付宝账号</label>
+                <div class="col-md-6">
+                  {{ $orderData['alipay'] }}
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">艾滋病检测结果</label>
-              <div class="col-md-6">
-                <select class="form-control input-inline input-medium adis_result">
-                  <option value="0">未检测</option>
-                  <option value="1" {{$orderData['adis_result']==1?'selected':''}}>阴性</option>
-                  <option value="2" {{$orderData['adis_result']==2?'selected':''}}>阳性</option>
-                </select>
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">艾滋病检测结果</label>
+                <div class="col-md-6">
+                  <select class="form-control input-inline input-medium adis_result">
+                    <option value="0">未检测</option>
+                    <option value="1" {{$orderData['adis_result']==1?'selected':''}}>阴性</option>
+                    <option value="2" {{$orderData['adis_result']==2?'selected':''}}>阳性</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">梅毒检测结果</label>
-              <div class="col-md-6">
-                <select class="form-control input-inline input-medium syphilis_result">
-                  <option value="0">未检测</option>
-                  <option value="1" {{$orderData['syphilis_result']==1?'selected':''}}>阴性</option>
-                  <option value="2" {{$orderData['syphilis_result']==2?'selected':''}}>阳性</option>
-                </select>
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">梅毒检测结果</label>
+                <div class="col-md-6">
+                  <select class="form-control input-inline input-medium syphilis_result">
+                    <option value="0">未检测</option>
+                    <option value="1" {{$orderData['syphilis_result']==1?'selected':''}}>阴性</option>
+                    <option value="2" {{$orderData['syphilis_result']==2?'selected':''}}>阳性</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">甲肝检测结果</label>
-              <div class="col-md-6">
-                <select class="form-control input-inline input-medium hepatitis_b_result">
-                  <option value="0">未检测</option>
-                  <option value="1" {{$orderData['hepatitis_b_result']==1?'selected':''}}>阴性</option>
-                  <option value="2" {{$orderData['hepatitis_b_result']==2?'selected':''}}>阳性</option>
-                </select>
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">甲肝检测结果</label>
+                <div class="col-md-6">
+                  <select class="form-control input-inline input-medium hepatitis_b_result">
+                    <option value="0">未检测</option>
+                    <option value="1" {{$orderData['hepatitis_b_result']==1?'selected':''}}>阴性</option>
+                    <option value="2" {{$orderData['hepatitis_b_result']==2?'selected':''}}>阳性</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">乙肝检测结果</label>
-              <div class="col-md-6">
-                <select class="form-control input-inline input-medium hepatitis_c_result">
-                  <option value="0">未检测</option>
-                  <option value="1" {{$orderData['hepatitis_c_result']==1?'selected':''}}>阴性</option>
-                  <option value="2" {{$orderData['hepatitis_c_result']==2?'selected':''}}>阳性</option>
-                </select>
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">乙肝检测结果</label>
+                <div class="col-md-6">
+                  <select class="form-control input-inline input-medium hepatitis_c_result">
+                    <option value="0">未检测</option>
+                    <option value="1" {{$orderData['hepatitis_c_result']==1?'selected':''}}>阴性</option>
+                    <option value="2" {{$orderData['hepatitis_c_result']==2?'selected':''}}>阳性</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">检测医生</label>
-              <div class="col-md-6">
-                <input class="form-control " type="text" name="check_doctor" id="check_doctor" value="{{$orderData['check_doctor']}}" placeholder="检测医生">
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">检测医生</label>
+                <div class="col-md-6">
+                  <input class="form-control " type="text" name="check_doctor" id="check_doctor" value="{{$orderData['check_doctor']}}" placeholder="检测医生">
+                </div>
               </div>
-            </div>
-            <div class="col-xs-12">
-              <label class="col-md-3 control-label">检测描述</label>
-              <div class="col-md-6">
-                <textarea class="form-control " cols="9" rel="3" id="check_desc">{{$orderData['check_desc']}}</textarea>
+              <div class="col-xs-12">
+                <label class="col-md-3 control-label">检测描述</label>
+                <div class="col-md-6">
+                  <textarea class="form-control " cols="9" rel="3" id="check_desc">{{$orderData['check_desc']}}</textarea>
+                </div>
+                <label class="col-md-3 control-label">
+                  <input type="button" class="input-group-btn btn btn-default btn-sm input-small save" value="保存" style="background: #3fd5c0;color: #ffffff;">
+                </label>
               </div>
-              <label class="col-md-3 control-label">
-                <input type="button" class="input-group-btn btn btn-default btn-sm input-small save" value="保存" style="background: #3fd5c0;color: #ffffff;">
-              </label>
             </div>
           </div>
         </div>
       </div>
-    </div>
-      @endif
+    @endif
     <div class="col-xs-6">
       <div class="portlet blue-hoki box">
         <div class="portlet-title">
@@ -157,11 +157,15 @@
           <div class="row static-info">
             <div class="col-xs-4">订单状态</div>
             <div class="col-xs-8">
-              <select class="form-control input-inline input-medium order_status" {{ $userinfo->is_admin?'':'disabled' }} >
-                @foreach($orderStatus as $k=>$v)
-                  <option value="{{$k}}" {{ $orderData['order_status']==$k?'selected':'' }}>{{$v}}</option>
-                @endforeach
-              </select>
+              @if($userinfo->is_admin)
+                <select class="form-control input-inline input-medium order_status" {{ $userinfo->is_admin?'':'disabled' }} >
+                  @foreach($orderStatus as $k=>$v)
+                    <option value="{{$k}}" {{ $orderData['order_status']==$k?'selected':'' }}>{{$v}}</option>
+                  @endforeach
+                </select>
+              @else
+                {{$orderStatus[$orderData['order_status']] or ''}}
+              @endif
             </div>
           </div>
           <div class="row static-info">
@@ -177,44 +181,42 @@
 
 @endsection
 @push('head-style')
-  <style type="text/css">
-
-  </style>
+  <style type="text/css"></style>
 @endpush
 @push('foot-script')
-    <script src="{{gStatic('vendor/plugins/qiniu/plupload.min.js')}}" ></script>
-    <script src="{{gStatic('vendor/plugins/plupload/i18n/zh_CN.js')}}" ></script>
-    <script src="{{gStatic('vendor/plugins/qiniu/qiniu.min.js')}}" ></script>
-    <script src="{{gStatic('vendor/plugins/qiniu/progress.js')}}" ></script>
-    <script>
+  <script src="{{gStatic('vendor/plugins/qiniu/plupload.min.js')}}"></script>
+  <script src="{{gStatic('vendor/plugins/plupload/i18n/zh_CN.js')}}"></script>
+  <script src="{{gStatic('vendor/plugins/qiniu/qiniu.min.js')}}"></script>
+  <script src="{{gStatic('vendor/plugins/qiniu/progress.js')}}"></script>
+  <script>
       $(function () {
           $(".up_images").click(function () {
               uploader();
           });
           $("#save_up_images").click(function () {
               var images = '';
-               $('.input_images img').each(function () {
+              $('.input_images img').each(function () {
                   images += $(this).attr('src') + ',';
               });
-               if( images ){
-                   $.post("{{yUrl(['site/payimages'])}}",{
-                       'user_id':"{{$orderData['uid']}}",
-                       'uuid':"{{$orderData['uuid']}}",
-                       'images':images
-                   },function (res) {
-                       if(res.code == 200){
-                           layer.msg('保存成功！！', {'icon': 1, time: 1200}, function () {
-                               location.reload();
-                           });
-                       }
-                   });
-               }
+              if (images) {
+                  $.post("{{yUrl(['site/payimages'])}}", {
+                      'user_id': "{{$orderData['uid']}}",
+                      'uuid': "{{$orderData['uuid']}}",
+                      'images': images
+                  }, function (res) {
+                      if (res.code == 200) {
+                          layer.msg('保存成功！！', {'icon': 1, time: 1200}, function () {
+                              location.reload();
+                          });
+                      }
+                  });
+              }
           });
           examine_reason();
-          $('#apply_result').change(function(){
+          $('#apply_result').change(function () {
               examine_reason();
           });
-          $('.examine_save').click(function(){
+          $('.examine_save').click(function () {
               $.post("{{yUrl(['site/applydeal'])}}",
                   {
                       'uuid': "{{$orderData['uuid']}}",
@@ -268,13 +270,15 @@
               );
           });
       });
+
       function examine_reason() {
-          if( $("#apply_result").val() == 2 ){
-              document.getElementById("examine_reason").style.display="block";
-          }else{
-              document.getElementById("examine_reason").style.display="none";
+          if ($("#apply_result").val() == 2) {
+              document.getElementById("examine_reason").style.display = "block";
+          } else {
+              document.getElementById("examine_reason").style.display = "none";
           }
       }
+
       function uploader(id) {
           return Qiniu.uploader({
               runtimes: 'html5,flash,html4',    //上传模式,依次退化
@@ -311,7 +315,7 @@
                   'FileUploaded': function (up, file, info) {
                       var res = JSON.parse(info);
                       console.log(res.key);
-                      var input_image = '<a href="{{env('QINIU_DOMAIN')}}/'+res.key+'" target="_blank"> <img src="{{env('QINIU_DOMAIN')}}/'+res.key+'" width="200"/> </a>';
+                      var input_image = '<a href="{{env('QINIU_DOMAIN')}}/' + res.key + '" target="_blank"> <img src="{{env('QINIU_DOMAIN')}}/' + res.key + '" width="200"/> </a>';
 
                       $('.input_images').append(input_image);
                   },
@@ -321,6 +325,5 @@
               }
           });
       }
-
-    </script>
+  </script>
 @endpush

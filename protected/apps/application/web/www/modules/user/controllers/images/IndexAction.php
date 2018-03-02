49f121add1b4f131cac6283eb30d26ebe842e481
gouki <gouki.xiao@gmail.com>
2018-03-03 01:37:01 +0800

    更新模型！

--- protected/apps/application/web/www/modules/user/controllers/images/IndexAction.php
+++ protected/apps/application/web/www/modules/user/controllers/images/IndexAction.php
@@ -1,29 +1,44 @@
 <?php
+
 namespace application\web\www\modules\user\controllers\images;
+
 use application\models\base\OrderList;
 use application\models\base\PayImage;
 use application\web\www\components\WwwBaseAction;
 
-class IndexAction extends WwwBaseAction{
-    public function run(){
-        if( \Yii::$app->request->isPost ){
+class IndexAction extends WwwBaseAction
+{
+    public function run()
+    {
+        if(\Yii::$app->request->isPost){
             \Yii::$app->response->format = 'json';
             $postData = \Yii::$app->request->post();
-            $orderModel = OrderList::find()->andWhere(['uuid'=>$postData['order_uuid']])->one();
+            $orderModel = OrderList::find()
+                                   ->andWhere(['uuid' => $postData['order_uuid']])
+                                   ->one();
             $orderModel->is_up_result = 1;
-            $imageArr  = explode(',',trim($postData['images'],','));
-            foreach ( $imageArr as $k=>$v ){
+            $imageArr = explode(',', trim($postData['images'], ','));
+            $trans = \Yii::$app->db->beginTransaction();
+            $erros = [];
+            foreach($imageArr as $k => $v){
                 $imageModel = new PayImage();
                 $imageModel->user_id = $this->account['uid'];
                 $imageModel->order_uuid = $postData['order_uuid'];
                 $imageModel->image = $v;
                 $imageModel->created_at = date('Y-m-d H:i:s');
                 $imageModel->save();
+                if($imageModel->hasErrors()){
+                    $trans->rollBack();
+                    $erros[] = $imageModel->getErrors();
+                    break;
+                }
             }
-            if( $orderModel->save()) {
+            if($orderModel->save()){
+                $trans->commit();
                 return ['code' => 200];
             }
-            return ['code'=>500,'message'=>$orderModel->getErrors()];
+            $trans->rollBack();
+            return ['code' => 500, 'message' => [$orderModel->getErrors(), $erros]];
         }
         return $this->render(compact(''));
     }

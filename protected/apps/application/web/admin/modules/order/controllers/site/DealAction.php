09d3726c8a8859ddcb9ccf7ea0ae1c0f85394da3
1915880948 <1915880948@qq.com>
2018-03-26 18:28:17 +0800

    导出样式

--- protected/apps/application/web/admin/modules/order/controllers/site/DealAction.php
+++ protected/apps/application/web/admin/modules/order/controllers/site/DealAction.php
@@ -22,9 +22,30 @@ class DealAction extends AdminBaseAction
             if($postData['method'] == 'deal_check'){
                 $order->order_updated_at = date("Y-m-d H:i:s");
                 $order->adis_result = $postData['adis_result'];
+                $order->adis_is_confirm = $postData['adis_is_confirm'];
+                $order->adis_confirm_time = $postData['adis_confirm_time'];
+                $order->adis_is_cure = $postData['adis_is_cure'];
+                $order->adis_cure_time = $postData['adis_cure_time'];
+
                 $order->syphilis_result = $postData['syphilis_result'];
+                $order->syphilis_is_confirm = $postData['syphilis_is_confirm'];
+                $order->syphilis_confirm_time = $postData['syphilis_confirm_time'];
+                $order->syphilis_is_cure = $postData['syphilis_is_cure'];
+                $order->syphilis_cure_time = $postData['syphilis_cure_time'];
+
+
                 $order->hepatitis_b_result = $postData['hepatitis_b_result'];
+                $order->hepatitis_b_is_confirm = $postData['hepatitis_b_is_confirm'];
+                $order->hepatitis_b_confirm_time = $postData['hepatitis_b_confirm_time'];
+                $order->hepatitis_b_is_cure = $postData['hepatitis_b_is_cure'];
+                $order->hepatitis_b_cure_time = $postData['hepatitis_b_cure_time'];
+
                 $order->hepatitis_c_result = $postData['hepatitis_c_result'];
+                $order->hepatitis_c_is_confirm = $postData['hepatitis_c_is_confirm'];
+                $order->hepatitis_c_confirm_time = $postData['hepatitis_c_confirm_time'];
+                $order->hepatitis_c_is_cure = $postData['hepatitis_c_is_cure'];
+                $order->hepatitis_c_cure_time = $postData['hepatitis_c_cure_time'];
+
                 $order->check_doctor = $postData['check_doctor'];
                 $order->check_desc = $postData['check_desc'];
                 $order->check_time = date('Y-m-d H:i:s', time());
@@ -64,6 +85,17 @@ class DealAction extends AdminBaseAction
             OrderList::ORDER_STATUS_REFUND_FINISHED       => '退款完成',
             OrderList::ORDER_STATUS_FINISHED              => '订单完成'
         ];
-        return $this->render(compact('images', 'orderData', 'orderStatus'));
+        $checkArr = [
+            '0'   => '未检测',
+            '1'   => '阴性',
+            '2'   => '阳性',
+            '3'   => '检测失败'
+        ];
+        $isNotArr = [
+            '0'   => '否',
+            '1'   => '是'
+        ];
+
+        return $this->render(compact('images', 'orderData', 'orderStatus','checkArr','isNotArr'));
     }
 }
